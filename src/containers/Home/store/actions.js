4350f6d08e31a71ea63106f4a6bce1a6e92d1bc3
GanYihuan <583520052@qq.com>
2019-05-01 23:01:30 +0800

    redux-thunk withExtraArgument

--- src/containers/Home/store/actions.js
+++ src/containers/Home/store/actions.js
@@ -1,8 +1,8 @@
-﻿import axios from 'axios'
+﻿// import axios from 'axios'
 import config from '../../../config'
 import {CHANGE_LIST} from './constants'
-import clientAxios from '../../../client/request'
-import serverAxios from '../../../server/request'
+// import clientAxios from '../../../client/request'
+// import serverAxios from '../../../server/request'
 
 // action creator
 const changeList = (list) => ({
@@ -11,17 +11,23 @@ const changeList = (list) => ({
 })
 
 export const getHomeList = (server) => { // async
-  const request = server ? serverAxios : clientAxios
-  return (dispatch) => {
+  // const request = server ? serverAxios : clientAxios
+  return (dispatch, getState, axiosInstance) => { // axiosInstance： withExtraArgument 传递的参数
     // return promises result
-    return request
-      // .get('http://47.95.113.63/ssr/api/news.json?secret=`${config.secret}`')
-      // .get('http://47.95.113.63/ssr/api/news.json?secret=PP87ANTIPIRATE')
+    // return request
+    //   // .get('http://47.95.113.63/ssr/api/news.json?secret=`${config.secret}`')
+    //   // .get('http://47.95.113.63/ssr/api/news.json?secret=PP87ANTIPIRATE')
+    //   .get('/api/news.json?secret=PP87ANTIPIRATE')
+    //   .then((res) => {
+    //     const list = res.data.data
+    //     dispatch(changeList(list))
+    //     // console.log(res.data.data, 'res.data')
+    //   })
+    return axiosInstance
       .get('/api/news.json?secret=PP87ANTIPIRATE')
       .then((res) => {
         const list = res.data.data
         dispatch(changeList(list))
-        // console.log(res.data.data, 'res.data')
       })
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -5,16 +5,18 @@
 } from 'redux'
 import thunk from 'redux-thunk'
 import {reducer as homeReducter} from '../containers/Home/store'
+import clientAxios from '../client/request'
+import serverAxios from '../server/request'
 
 const reducer = combineReducers({
   home: homeReducter
 })
 
 export const getStore = () => {
-  return createStore(reducer, applyMiddleware(thunk))
+  return createStore(reducer, applyMiddleware(thunk.withExtraArgument(serverAxios)))
 }
 
 export const getClientStore = () => {
   const defaultState = window.context.state // 服务器端 window.context 数据注水; 客户端 window.context 数据脱水
-  return createStore(reducer, defaultState, applyMiddleware(thunk))
+  return createStore(reducer, defaultState, applyMiddleware(thunk.withExtraArgument(clientAxios)))
 }
